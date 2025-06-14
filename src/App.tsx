899a6189d2007e6b6ad1dbed940d818abe41ee8c
ishwarid <ishwaricarver@gmail.com>
2025-06-15 00:00:57 +0530

    build fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 import Sidebar from "./components/Sidebar";
-import PromptInput from "./components/PromptInput";
-import ChatMessage from "./components/ChatMessage";
 import RightPanel from "./components/RightPanel";
 import ChatPromptCard from "./components/ChatPromptCard";
 import TopNavbar from "./components/TopNavbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 import ApiKeyPage from "./pages/ApiKeyPage";
 
 
 function App() {
-  const messages = [
-    { sender: "user", text: "Explain the probability of rolling 7" },
-    {
-      sender: "ai",
-      text: "The probability of rolling a 7 with two six-sided dice is 6/36 or 1/6, since there are 6 combinations that yield a 7.",
-    },
-  ];
 
    return (
     <div className="flex flex-col h-screen bg-surface text-white">
